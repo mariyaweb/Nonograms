7e8c9405ae75428e10c0a4c36b819bfbf9a01e28
github-mariyaweb <96mariyask@gmail.com>
2024-01-31 15:33:16 +0300

    feat: add application controls

--- nonograms/index.js
+++ nonograms/index.js
@@ -1,5 +1,77 @@
 import games from './js/games.js';
 console.log(games);
+const size = games[10].size;
+console.log(size);
+const countCluesTopCols = size;
+const controlField = games[10].field.flat();
+console.log(controlField);
+
+
+// Create Header
+document.body.className = 'body';
+const header = document.createElement('header');
+header.className = 'header';
+header.innerHTML = `
+<div class="wrapper">
+<div class="header__row">
+  <div class="header__btns"></div>
+  <div class="header__theme theme">
+    <div class="theme_btn theme__btn-sun">
+      <span class="theme__icon icon-sun"></span>
+    </div>
+    <div class="theme_btn theme__btn-moon">
+      <span class="theme__icon icon-moon"></span>
+    </div>
+  </div>
+</div>
+</div>
+`;
+document.body.prepend(header);
+
+function createHeaderMenu() {
+  const headerBtns = document.querySelector('.header__btns');
+  const headerIco = ['folder', 'puzzle', 'light', 'star', 'random'];
+  headerIco.forEach(item => {
+    const btn = document.createElement('div');
+    btn.className = 'header__btn';
+    btn.id = item;
+    const ico = document.createElement('span');
+    ico.className = `header__icon icon-${item}`;
+    btn.append(ico);
+    headerBtns.append(btn);
+  })
+}
+createHeaderMenu();
+
+// Create Nonogram
+const nonogramSection = document.createElement('section');
+nonogramSection.className = 'nonogram';
+nonogramSection.innerHTML = `
+<div class="wrapper">
+      <div class="nonogram__row">
+        <h1 class="nonogram__title">Nonogram<span class="nonogram__icon icon-sparkles"></span>
+        </h1>
+        <div class="nonogram__timer"><span class="nonogram__min">00</span> : <span class="nonogram__sec">00</span>
+        </div>
+        <h2 class="nonogram__subtitle">${games[10].name}</h2>
+        <div class="nonogram__table">
+          <div class="nonogram__btns">
+          <div class="nonogram__btn btn-save">Save</div>
+          <div class="nonogram__btn btn-reset">Reset</div>
+        </div>
+      </div>
+</div>
+`
+document.body.append(nonogramSection);
+
+const saveBtn = document.querySelector('.btn-save');
+saveBtn.addEventListener('click', (e) => saveGame(e));
+
+const resetBtn = document.querySelector('.btn-reset');
+resetBtn.addEventListener('click', (e) => resetGame(e));
+
+
+// Nonogram Table
 const conteiner = document.createElement('div');
 conteiner.className = 'container';
 const tableContainer = document.createElement('table');
@@ -10,14 +82,8 @@ const tableCluesLeft = document.createElement('table');
 tableCluesLeft.className = 'table__clues_left table';
 const tablePlayField = document.createElement('table');
 tablePlayField.className = 'table__field table';
-const countCluesTopRows = games[0].cluesTop.length;
-const countCluesLeftCols = games[0].cluesLeft[0].length;
-
-const size = games[0].size;
-console.log(size);
-const countCluesTopCols = size;
-const controlField = games[0].field.flat();
-console.log(controlField);
+const countCluesTopRows = games[10].cluesTop.length;
+const countCluesLeftCols = games[10].cluesLeft[0].length;
 
 function createTable(container, rows, cols, classNameRow, classNameCol) {
   for (let i = 0; i < rows; i++) {
@@ -46,9 +112,11 @@ function createTable(container, rows, cols, classNameRow, classNameCol) {
   }
 }
 
+
+
 //Create table container
 createTable(tableContainer, 2, 2, 'table__container_row', 'table__container_col');
-document.body.appendChild(tableContainer);
+document.querySelector('.nonogram__btns').before(tableContainer);
 
 // Create top clues table
 function createTopClues() {
@@ -70,6 +138,47 @@ function createPlayField() {
   document.getElementsByClassName('table__container_row')[1].children[1].appendChild(tablePlayField);
 }
 createPlayField();
+
+//Create level list
+const nonogramList = document.createElement('div');
+nonogramList.className = 'nonogram__list level';
+const levelTitle = document.createElement('h2');
+levelTitle.className = 'level__title';
+levelTitle.innerHTML = 'Сhoose a nonogram';
+nonogramList.append(levelTitle);
+const levelBody = document.createElement('div');
+levelBody.className = 'level__body';
+document.body.append(nonogramList);
+
+function createLevelItem() {
+  const level = ['Easy', 'Medium', 'Hard'];
+  level.forEach((item, index) => {
+    const levelItem = document.createElement('div');
+    levelItem.className = 'level__item';
+    const levelSubtitle = document.createElement('h3');
+    levelSubtitle.className = 'level__subtitle';
+    levelSubtitle.innerHTML = `${item}`;
+    levelItem.append(levelSubtitle);
+    const btns = document.createElement('div');
+    btns.className = 'level__btns';
+
+    games.forEach(game => {
+      if (game.size === (index + 1) * 5) {
+        const btn = document.createElement('div');
+        btn.className = 'level__btn';
+        btn.innerHTML = `${game.name}`;
+        btns.append(btn);
+      }
+    });
+
+    levelItem.append(levelSubtitle);
+    levelItem.append(btns);
+    levelBody.append(levelItem);
+  });
+
+  nonogramList.append(levelBody);
+}
+createLevelItem();
 
 function createClues(item) {
   const { size, name, cluesTop, cluesLeft } = games[item];
@@ -94,7 +203,7 @@ function createClues(item) {
     });
   });
 }
-createClues(0);
+createClues(10);
 
 const changeFill = (e) => {
   const el = e.target;
@@ -142,10 +251,24 @@ function createModalWin(time) {
   modal.getElementsByClassName('modal__btn')[0].addEventListener('click', () => closeModalWin());
 }
 
-
-
 function newGame() {
   const modal = document.getElementsByClassName('modal')[0];
   modal.classList.remove('modal-show');
   modal.classList.add('modal-hide');
+}
+
+//Change Theme
+const root = document.querySelector(':root');
+const changeThemeBtn = document.querySelector('.header__theme');
+changeThemeBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  root.classList.toggle('dark');
+})
+
+function saveGame(e) {
+  console.log('save game');
+}
+
+function resetGame(e) {
+  console.log('reset game');
 }
